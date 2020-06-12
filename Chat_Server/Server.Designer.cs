96d94428c0599dc8f5175bd27fbaee37ea8516d1
JuneeDinh <dinhttnhung9910@gmail.com>
2020-06-12 09:39:41 +0700

    first commit

---
+++ Chat_Server/Server.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Chat_Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMess = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.tbMessView = new System.Windows.Forms.TextBox();
+            this.lvClientConnect = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbNumClient = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbMess
+            // 
+            this.tbMess.Location = new System.Drawing.Point(35, 590);
+            this.tbMess.Margin = new System.Windows.Forms.Padding(4);
+            this.tbMess.Multiline = true;
+            this.tbMess.Name = "tbMess";
+            this.tbMess.Size = new System.Drawing.Size(843, 57);
+            this.tbMess.TabIndex = 1;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(909, 590);
+            this.btSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(100, 57);
+            this.btSend.TabIndex = 2;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 553);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Messages";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbPort);
+            this.groupBox1.Controls.Add(this.tbIP);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(78, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(393, 142);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(104, 98);
+            this.tbPort.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(233, 22);
+            this.tbPort.TabIndex = 3;
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(104, 42);
+            this.tbIP.Margin = new System.Windows.Forms.Padding(4);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(233, 22);
+            this.tbIP.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 98);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "IP";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(507, 69);
+            this.btStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(100, 49);
+            this.btStart.TabIndex = 5;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // tbMessView
+            // 
+            this.tbMessView.Location = new System.Drawing.Point(34, 174);
+            this.tbMessView.Multiline = true;
+            this.tbMessView.Name = "tbMessView";
+            this.tbMessView.Size = new System.Drawing.Size(610, 362);
+            this.tbMessView.TabIndex = 6;
+            // 
+            // lvClientConnect
+            // 
+            this.lvClientConnect.HideSelection = false;
+            this.lvClientConnect.Location = new System.Drawing.Point(684, 174);
+            this.lvClientConnect.Name = "lvClientConnect";
+            this.lvClientConnect.Size = new System.Drawing.Size(325, 362);
+            this.lvClientConnect.TabIndex = 7;
+            this.lvClientConnect.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(681, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tổng số client kết nối:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(688, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Các client kết nối:";
+            // 
+            // tbNumClient
+            // 
+            this.tbNumClient.Location = new System.Drawing.Point(684, 70);
+            this.tbNumClient.Multiline = true;
+            this.tbNumClient.Name = "tbNumClient";
+            this.tbNumClient.Size = new System.Drawing.Size(325, 48);
+            this.tbNumClient.TabIndex = 10;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 660);
+            this.Controls.Add(this.tbNumClient);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lvClientConnect);
+            this.Controls.Add(this.tbMessView);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.tbMess);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbMess;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.TextBox tbMessView;
+        private System.Windows.Forms.ListView lvClientConnect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbNumClient;
+    }
+}
+
---
+++ Chat_Server/Server.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat_Server
+{
+    public partial class Server : Form
+    {
+        private byte[] _buffer = new byte[1024];
+        public List<SocketM> __ClientSockets { get; set; }
+        List<string> _names = new List<string>();
+        private Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public Server()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            __ClientSockets = new List<SocketM>();
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lvClientConnect.SelectedItems.Count; i++)
+            {
+                string t = lvClientConnect.SelectedItems[i].ToString();
+                for (int j = 0; j < __ClientSockets.Count; j++)
+                {
+                    //if (__ClientSockets[j]._Socket.Connected && __ClientSockets[j]._Name.Equals("@"+t))
+                    {
+                        Sendata(__ClientSockets[j]._Socket, tbMess.Text);
+                    }
+                }
+            }
+            tbMessView.AppendText("\nServer: " + tbMess.Text);
+        }
+        private void SetupServer()
+        {
+            //lb_stt.Text = "Setting up server . . .";
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Parse(tbIP.Text), int.Parse(tbPort.Text)));
+            _serverSocket.Listen(1);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        private void AppceptCallback(IAsyncResult ar)
+        {
+            Socket socket = _serverSocket.EndAccept(ar);
+            __ClientSockets.Add(new SocketM(socket));
+            lvClientConnect.Items.Add(socket.RemoteEndPoint.ToString());
+
+            tbNumClient.Text = "Số client đang kết nối: " + __ClientSockets.Count.ToString();
+            tbMessView.Text = "Client connected. . .";
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            if (socket.Connected)
+            {
+                int received;
+                try
+                {
+                    received = socket.EndReceive(ar);
+                }
+                catch (Exception)
+                {
+                    // client đóng kết nối
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            __ClientSockets.RemoveAt(i);
+                            tbNumClient.Text =__ClientSockets.Count.ToString();
+                        }
+                    }
+                    // xóa trong list
+                    return;
+                }
+                if (received != 0)
+                {
+                    byte[] dataBuf = new byte[received];
+                    Array.Copy(_buffer, dataBuf, received);
+                    string text = Encoding.ASCII.GetString(dataBuf);
+                    tbMessView.Text = "Text received: " + text;
+
+                    string reponse = string.Empty;
+                    //if (text.Contains("@@"))
+                    //{
+                    //    for (int i = 0; i < list_Client.Items.Count; i++)
+                    //    {
+                    //        if (socket.RemoteEndPoint.ToString().Equals(__ClientSockets[i]._Socket.RemoteEndPoint.ToString()))
+                    //        {
+                    //            list_Client.Items.RemoveAt(i);
+                    //            list_Client.Items.Insert(i, text.Substring(1, text.Length - 1));
+                    //            __ClientSockets[i]._Name = text;
+                    //            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+                    //            return;
+                    //        }
+                    //    }
+                    //}
+
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (socket.RemoteEndPoint.ToString().Equals(__ClientSockets[i]._Socket.RemoteEndPoint.ToString()))
+                        {
+                            tbMess.AppendText("\n" + __ClientSockets[i]._Name + ": " + text);
+                        }
+                    }
+
+
+
+                    if (text == "bye")
+                    {
+                        return;
+                    }
+                    reponse = "\nServer da nhan " + text;
+                    Sendata(socket, reponse);
+                }
+                else
+                {
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            __ClientSockets.RemoveAt(i);
+                            tbNumClient.Text = __ClientSockets.Count.ToString();
+                        }
+                    }
+                }
+            }
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+        void Sendata(Socket socket, string noidung)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(noidung);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        private void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            SetupServer();
+        }
+    }
+}
